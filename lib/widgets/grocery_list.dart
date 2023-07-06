d4f8fc95396820e9b812f14dd15036f63b58303d
lelouch248 <lelouch248@users.noreply.github.com>
2023-07-06 10:28:23 +0530

    section 11 challenge complete

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,30 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_data.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItem = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+    if (newItem != null) {
+      setState(() {
+        _groceryItem.add(newItem);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Widget groceryListContent;
+
+    if (_groceryItem.isEmpty) {
+      groceryListContent = Center(
+        child: Text(
+          'No Groceries Yet! Please add some!',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary, fontSize: 20),
+        ),
+      );
+    } else {
+      groceryListContent = ListView.builder(
+        itemCount: _groceryItem.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Theme.of(context).colorScheme.error,
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.only(right: 20),
+              margin: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              child: const Icon(
+                Icons.delete,
+                color: Colors.red,
+                size: 40,
+              ),
+            ),
+            onDismissed: (direction) {
+              setState(() {
+                _groceryItem.removeAt(index);
+              });
+            },
+            key: ValueKey(_groceryItem[index].id),
+            child: ListTile(
+              title: Text(_groceryItem[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItem[index].category.color,
+              ),
+              trailing: Text(_groceryItem[index].quantity.toString()),
+            ),
+          );
+        },
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: groceryItems[index].category.color,
-            ),
-            trailing: Text(groceryItems[index].quantity.toString()),
-          );
-        },
-      ),
+      body: groceryListContent,
     );
   }
 }
---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+  var _enteredname = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
+
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.of(context).pop(GroceryItem(
+          id: DateTime.now().toString(),
+          name: _enteredname,
+          quantity: _enteredQuantity,
+          category: _selectedCategory));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add New Item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                autocorrect: true,
+                validator: (value) {
+                  if (value!.isEmpty ||
+                      value.length < 3 ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Please enter at least 3 characters';
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  _enteredname = newValue!;
+                },
+                decoration: const InputDecoration(labelText: 'Item Name'),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(labelText: 'Quantity'),
+                      initialValue: '1',
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid Positive number ';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        _enteredQuantity = int.parse(newValue!);
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      value: _selectedCategory,
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveItem,
+                    child: const Text('Add Item'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
